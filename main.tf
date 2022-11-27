5c70b30457531f7ec7a4acb7e7c676cdd9671523
Baswaraju Vuppala <bvuppala@gmail.com>
2022-11-26 18:30:02 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,3 +5,8 @@ resource "azurerm_resource_group" "myname" {
   name = "tfcloud_rg"
   location = "eastus"
 }
+
+resource "azurerm_resource_group" "myname2" {
+  name = "tfcloud_rg2"
+  location = "westus"
+}
